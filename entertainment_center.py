b86cbd3719869441b054122bb55edab2f202bb5b
AJ Gray <awgray@AJs-MacBook-Pro.fios-router.home>
2017-11-10 16:39:22 -0500

    Add movie objects

---
+++ entertainment_center.py
@@ -0,0 +1,26 @@
+from media import Movie
+
+star_wars = Movie("Star Wars: Empire Strikes Back",
+                  "https://images-na.ssl-images-amazon.com/images/I/91%2BCydthCeL._SY445_.jpg",
+                  "https://www.youtube.com/watch?v=JNwNXF9Y6kY")
+
+pirates_1 = Movie("Pirates of the Caribbean: Curse of the Black Pearl",
+                  "https://i.jeded.com/i/pirates-of-the-caribbean-1-the-curse-of-the-black-pearl.12323.jpg",
+                  "https://www.youtube.com/watch?v=naQr0uTrH_s")
+
+lotr = Movie("Lord of The Rings: The Two Towers",
+             "https://upload.wikimedia.org/wikipedia/en/a/ad/Lord_of_the_Rings_-_The_Two_Towers.jpg",
+             "https://www.youtube.com/watch?v=LbfMDwc4azU")
+
+cap_1 = Movie("Captain America: The First Avenger",
+              "https://upload.wikimedia.org/wikipedia/en/3/37/Captain_America_The_First_Avenger_poster.jpg",
+              "https://www.youtube.com/watch?v=JerVrbLldXw")
+
+avengers_2 = Movie("Avengers: Age of Ultron",
+                   "https://upload.wikimedia.org/wikipedia/en/1/1b/Avengers_Age_of_Ultron.jpg",
+                   "https://www.youtube.com/watch?v=tmeOjFno6Do")
+
+thor_3 = Movie("Thor: Ragnarok",
+               "https://images-na.ssl-images-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
+               "https://www.youtube.com/watch?v=ue80QwXMRHg")
+
