a824132fb86ad291804b0b595f5b6b1c6720aef6
AJ Gray <awgray@AJs-MacBook-Pro.fios-router.home>
2017-11-10 16:43:06 -0500

    Remove 'object' paren from class creation

--- media.py
+++ media.py
@@ -1,8 +1,7 @@
-class Movie(object):
+class Movie:
 
     def __init__(self, title, post_url, trailer):
         self.title = title
         self.post_url = post_url
         self.trailer = trailer
-
 
