81fcd12d5c7997e8e7c2d111c201a136966e24cf
AJ Gray <awgray@AJs-MacBook-Pro.fios-router.home>
2017-11-11 16:36:31 -0500

    Revise README to adhere to requirements

--- media.py
+++ media.py
@@ -1,6 +1,12 @@
 class Movie:
 
-    def __init__(self, title, poster, trailer):
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+
+        """
+        :param title: Readable string that gives title of movie
+        :param poster_image_url: Image that depicts the movie cover
+        :param trailer_youtube_url: Video that gives preview of movie
+        """
         self.title = title
-        self.poster_image_url = poster
-        self.trailer_youtube_url = trailer
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
